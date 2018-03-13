ef74093364739cadf3c4c34c4b7d49b8884b0411
James Arista Yaputra <jamesyaputra1704@gmail.com>
2018-03-13 20:08:17 +0800

    As of 13/03/2018

--- whatdoesthefoxsay.cpp
+++ whatdoesthefoxsay.cpp
@@ -6,21 +6,19 @@ using namespace std;
 
 int main () {
 	int test;
-	vector<string> input, sound, output;
 	string in;
+
 	cin >> test;
-	
 	for (int i = 0; i < test; i++) {
-		input.clear();
-		sound.clear();
-		output.clear();
+		vector<string> input, sound, output;
+		string token;
+		int flag;
 		do {
 			cin >> in;
 			input.push_back(in);
 		} while (cin.get() != '\n');
 
-		string token;
-		do {
+		while(1) {
 			cin >> in;
 			istringstream ss(in);
 			while(getline(ss, token)) {
@@ -30,24 +28,17 @@ int main () {
 					sound.push_back(in);
 				}
 			}
-		} while (token != "say?");
+		}
 		
-		int flag;
 		for (int j = 0; j < input.size(); j++) {
 			flag = 0;
 			for (int k = 0; k < sound.size(); k++) {
 				if (input[j] == sound[k]) flag = 1;
 			}
-			if (!flag) {
-				output.push_back(input[j]);
-			}
+			if (!flag) output.push_back(input[j]);
 		}
-
-		for (int j = 0; j < output.size(); j++) {
-			cout << output[j] << " ";
-		}
+		for (int j = 0; j < output.size(); j++) cout << output[j] << " ";
 		cout << endl;
 	}
-
 	return 0;
 }
