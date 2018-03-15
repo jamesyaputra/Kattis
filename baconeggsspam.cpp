20939209b7d5e7c86a0fe80b9bc1621a190199af
James Arista Yaputra <jamesyaputra1704@gmail.com>
2018-03-16 00:21:21 +0800

    As of 16/03/2018

--- baconeggsspam.cpp
+++ baconeggsspam.cpp
@@ -3,35 +3,26 @@ using namespace std;
 
 int main () {
 	int cases;
-	string name, in;
-	map<string, vector<string> > menu;
-	vector<string> input;
-	while (1) {
-		cin >> cases;
-		if (!cases) break;
-		menu.clear();
-		while(cases--) {
-			input.clear();
-			cin >> name;
-			do {
-				cin >> in;
-				input.push_back(in);
-			} while (cin.get() != '\n');
 
-			for (auto it = input.begin(); it != input.end(); it++) {
-				menu[*it].push_back(name);
-			}
+	while (cin >> cases, cases) {
+		cin.get();
+		map <string, set<string>> myMap;
+		for (int i = 0; i < cases; i++) {
+			string line, name, food;
+			getline(cin, line);
+			istringstream iss(line);
+			iss >> name;
+			while (iss >> food)
+				myMap[food].insert[name];
 		}
 
-		for (auto it = menu.begin(); it != menu.end(); it++) 
-			stable_sort(it->second.begin(), it->second.end());
-
-		for (auto& it : menu) {
-			cout << it.first << " ";
-			for (auto itt = it.second.begin(); itt != it.second.end(); itt++)
-				cout << *itt << " ";
-			cout << endl << endl;
+		for (auto &it : myMap) {
+			cout << it.first;
+			for (auto &itt : it.second)
+				cout << " " << itt;
+			cout << endl;
 		}
+		cout << endl;
 	}
 	return 0;
 }
--- compoundwords.cpp
+++ compoundwords.cpp
@@ -6,20 +6,16 @@ using namespace std;
 
 int main () {
 	string in;
-	//int cases;
 	vector <string> input;
 	set <string> output;
-	//cin >> cases;
 	while (cin >> in) {
-		//cin >> in;
 		for (auto it = input.begin(); it != input.end(); it++) {
-				output.insert(in + *it);
-				output.insert(*it + in);
+			output.insert(in + *it);
+			output.insert(*it + in);
 		}
 		input.push_back(in);
 	}
 
-	//stable_sort(output.begin(), output.end());
 	for (auto it = output.begin(); it != output.end(); it++)
 		cout << *it << endl;
 
