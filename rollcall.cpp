65b56743076d9e7b11fe049ccb5d7086b9baa875
James Arista Yaputra <jamesyaputra1704@gmail.com>
2018-03-23 00:00:06 +0800

    As of 23/03/2018

---
+++ rollcall.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main() {
+	string first, last;
+	map<string, vector<string> > names, lastNames;
+	int TC;
+
+	while (cin >> first >> last) {
+		names[first].push_back(last);
+	}
+
+	for (auto& it: names) {
+		for (auto& itt : it.second) {
+			if (it.second.size() > 1) lastNames[itt].push_back(it.first+" "+itt);
+			else lastNames[itt].push_back(it.first);
+		}
+	}
+
+	for (auto& it : lastNames) for (auto& itt : it.second) cout << itt << endl;
+
+	return 0;
+}
