ca063a168b3c66a557601e2c3975b760c4e25578
James Arista Yaputra <jamesyaputra1704@gmail.com>
2018-03-24 01:50:26 +0800

    As of 24/03/2018

---
+++ zipfsong.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int main () {
+	int songs, picks;
+	int plays, count, quality, ref;
+	string names;
+	map<int, string> list;
+
+	cin >> songs >> picks;
+
+	for (int i = 0; i < songs; i++) {
+		count = i+1;
+		cin >> plays >> names;
+		if (!i) ref = plays;
+		quality = plays/(ref/count);
+		list[]
+	}
+
+	for (int i = 0; i < songs; i++) {
+
+	}
+
+}
